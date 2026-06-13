55567c1ca2cec9d7321ef8f34ebfca60555be184
Suhas <suhasrp44@gmail.com>
2026-06-13 15:14:37 +0530

    Fixed SingletonPatternExample structure

---
+++ SingletonPatternExample/LoggerTest.java
@@ -0,0 +1,18 @@
+public class LoggerTest {
+
+    public static void main(String[] args) {
+
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+        logger1.log("Application Started");
+        logger2.log("User Logged In");
+
+        System.out.println(logger1);
+        System.out.println(logger2);
+
+        if (logger1 == logger2) {
+            System.out.println("Only one Logger instance exists.");
+        }
+    }
+}
